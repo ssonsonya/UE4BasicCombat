d761a1673d5f70fbfc579431f315066c72cc1270
Sonya <hyonseonkim@gmail.com>
2023-06-29 16:23:12 +0900

    Initial Commit

---
+++ Source/UE4BasicCombat/UE4BasicCombat.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4BasicCombat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4BasicCombat, "UE4BasicCombat" );
---
+++ Source/UE4BasicCombat/UE4BasicCombatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4BasicCombatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4BASICCOMBAT_API AUE4BasicCombatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
